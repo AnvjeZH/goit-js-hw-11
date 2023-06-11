d4941f9aacb835fe4cfd5b123eddfabe68e8882c
Anzhelika Zhupynas <121193138+AnvjeZH@users.noreply.github.com>
2023-06-11 20:02:38 +0300

    done

--- src/api-service/api.js
+++ src/api-service/api.js
@@ -1,20 +1,26 @@
-const API = '37188095-ba760fd787fe26f9993fba281';
-const BASE_URL = 'https://pixabay.com/api/';
+import axios from 'axios';
 
-export function fetchQuery(q, page) {
-    const options = new URLSearchParams({ key: API, 
+axios.defaults.baseURL = 'https://pixabay.com/api/';
+
+const API = '37188095-ba760fd787fe26f9993fba281';
+
+export async function fetchQuery(q, page) {
+  try {
+    const {data}= await axios({
+      params: {
+        key: API,
         q,
         image_type: 'photo',
-        orientation:'horizontal',
+        orientation: 'horizontal',
         safesearch: true,
         page,
-        per_page: 40
+        per_page: 40,
+      },
     });
-    return fetch(`${BASE_URL}?${options}`).then((response) => {
-        if(!response.ok) {
-            throw new Error(response.status)
-        }
-        return response.json()
-    })
-    .catch((error) => console.log(error))
+    return data
+  } catch (error) {
+    console.log(error);
+  }
+
+
 }
--- src/index.js
+++ src/index.js
@@ -1,86 +1,94 @@
-import axios, { all } from 'axios'
+import axios, { all } from 'axios';
 import Notiflix from 'notiflix';
-import {fetchQuery} from './api-service/api'
+import { fetchQuery } from './api-service/api';
 
-const form = document.querySelector('form')
-const gallery = document.querySelector('.gallery')
-const searchBtn = document.querySelector('[type="submit"]')
-const loadMore = document.querySelector('.load-more')
+const form = document.querySelector('form');
+const gallery = document.querySelector('.gallery');
+const searchBtn = document.querySelector('[type="submit"]');
+const loadMore = document.querySelector('.load-more');
 
-const perPage = 40
-let pageNumber = 1
-let querySearch = ''
+const perPage = 40;
+let pageNumber = 1;
+let querySearch = '';
 
-loadMore.classList.add('unvisible')
+loadMore.classList.add('unvisible');
 
-function getImages(keyword, page) {
-    fetchQuery(keyword, page).then((data) => {
-      console.log(data.hits.length)
-      if(data.totalHits === 0) {
-        Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.')
-        return
-      }
-        const imgArr = data.hits
-        renderMarkupImg(imgArr)
-        if (data.totalHits > 1 && data.totalHits > page * perPage) {
-          loadMore.classList.remove('unvisible')
-        } else {
-          loadMore.classList.add('unvisible')
-          Notiflix.Notify.warning("We're sorry, but you've reached the end of search results.")
-        }
-        
-    })
-    .catch(error => console.log(error))
-    .finally(() => {
-      // loadMore.classList.add('unvisible')
-    })
+async function getImages(keyword, page) {
+  const data = await fetchQuery(keyword, page);
+  if (data.totalHits === 0) {
+    Notiflix.Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+    return;
+  }
+  renderMarkupImg(data.hits);
+  if (pageNumber === 1) {
+    Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
+  }
+
+  if (data.totalHits > 1 && data.totalHits > page * perPage) {
+    loadMore.classList.remove('unvisible');
+  } else {
+    loadMore.classList.add('unvisible');
+    showNotification(
+      "We're sorry, but you've reached the end of search results."
+    );
+  }
 }
 
-
-
 function renderMarkupImg(imgArr) {
-    const markup = imgArr.map(({webformatURL, largeImageURL, tags, likes, views, comments, downloads}) => {
-        return `<div class="photo-card">
+  const markup = imgArr
+    .map(({ webformatURL, tags, likes, views, comments, downloads }) => {
+      return `<div class="photo-card">
         <img src="${webformatURL}" alt="${tags}" loading="lazy" />
         <div class="info">
           <p class="info-item">
-            <b>Likes ${likes}</b>
+            <b>Likes: ${likes}</b>
           </p>
           <p class="info-item">
-            <b>Views ${views}</b>
+            <b>Views: ${views}</b>
           </p>
           <p class="info-item">
-            <b>Comments ${comments}</b>
+            <b>Comments: ${comments}</b>
           </p>
           <p class="info-item">
-            <b>Downloads ${downloads}</b>
+            <b>Downloads: ${downloads}</b>
           </p>
         </div>
-      </div>`
+      </div>`;
     })
-    .join('')
-    gallery.insertAdjacentHTML('beforeend', markup)
+    .join('');
+  gallery.insertAdjacentHTML('beforeend', markup);
 }
 
-form.addEventListener('submit', onSearchImages)
+form.addEventListener('submit', onSearchImages);
 
 function onSearchImages(event) {
-  event.preventDefault()
-  inputValue = event.target.elements.searchQuery.value
-  if(!inputValue.trim() || inputValue === querySearch) {
-return
+  event.preventDefault();
+  inputValue = event.target.elements.searchQuery.value;
+  if (!inputValue.trim()) {
+    showNotification('Please enter your query in the search field');
+    return;
   }
-  querySearch = inputValue
-  pageNumber = 1
-  gallery.innerHTML = ''
-  loadMore.classList.add('unvisible')
-  getImages(querySearch, pageNumber)
+  if (inputValue === querySearch) {
+    showNotification('Request completed');
+    return;
+  }
+
+  querySearch = inputValue;
+  pageNumber = 1;
+  gallery.innerHTML = '';
+  loadMore.classList.add('unvisible');
+  getImages(querySearch, pageNumber);
 }
 
-loadMore.addEventListener('click', onLoadMore)
+loadMore.addEventListener('click', onLoadMore);
 
 function onLoadMore() {
-  loadMore.classList.add('unvisible')
-  pageNumber += 1
-  getImages(querySearch, pageNumber)
+  loadMore.classList.add('unvisible');
+  pageNumber += 1;
+  getImages(querySearch, pageNumber);
+}
+function showNotification(message) {
+  Notiflix.Notify.info(message);
 }
